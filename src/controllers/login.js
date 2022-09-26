43704e5b977549a22586fa02fd9e2ba3be8251c7
Gabriel <gabrielja2antunes@gmail.com>
2022-09-26 19:33:36 -0300

    chore: criando post controller

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -3,7 +3,7 @@ const loginService = require('../services/login');
 const authenticate = async (req, res, next) => {
   try {
     const token = await loginService.authenticate(req.body);
-    // console.log('token', token);
+
      return res.status(200).json(token);
   } catch (error) {
     next(error);
---
+++ src/controllers/post.js
@@ -0,0 +1,23 @@
+const postService = require('../services/post');
+const { authenticateToken } = require('../utils/JWT');
+
+const create = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    
+    req.userId = await authenticateToken(token);    
+    const post = await postService.createBlogPost(req.body);
+  
+    if (post === undefined) {
+      return res.status(400).json({ message: '"categoryIds" not found' });
+    }
+    const response = { ...post.dataValues, userId: req.userId.id };
+    return res.status(201).json(response); 
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  create,
+};
