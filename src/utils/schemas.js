40c90908af57d4a0bd87635915381e92169a17e8
Gabriel <gabrielja2antunes@gmail.com>
2022-09-26 19:41:15 -0300

    chore: criando  schema para o post

--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -12,7 +12,14 @@ const userSchema = Joi.object({
     image: Joi.string().min(10),
 });
 
+const postSchema = Joi.object({
+    title: Joi.string().required(),
+    content: Joi.string().required(),
+    categoryIds: Joi.array().required(),
+});
+
 module.exports = {
 loginSchema,
 userSchema,
+postSchema,
 };
