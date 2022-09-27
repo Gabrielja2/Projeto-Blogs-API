94e16ca7125ca30bb24deebe6cfe5819f8e59a7d
Gabriel <gabrielja2antunes@gmail.com>
2022-09-27 18:55:08 -0300

    fix: refatorando a autenticação da rota category

--- src/router/categories.js
+++ src/router/categories.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const categoryController = require('../controllers/categories');
-const authMiddleware = require('../middlewares/auth');
 
 const categoryRouter = express.Router();
 
-categoryRouter.post('/', authMiddleware, categoryController.create);
-categoryRouter.get('/', authMiddleware, categoryController.getCategories);
+categoryRouter.post('/', categoryController.create);
+categoryRouter.get('/', categoryController.getCategories);
 
 module.exports = categoryRouter;
